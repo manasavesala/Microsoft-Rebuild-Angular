9da9da7c109f8a08accaf0a592a7d2925781978f
Manasa Vesala <vesalamanasa@gmail.com>
2019-01-25 11:00:22 -0800

    Added images,css and html in components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AdBarComponent } from './ad-bar/ad-bar.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AdBarComponent
+    AdBarComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
